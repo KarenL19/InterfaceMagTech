bfca6df3603c18176033093759d8d497816d94cc
CarolineFontainha <cbsfontainha@gmail.com>
2020-07-16 22:21:24 -0300

    first component

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View, Image} from 'react-native';
+import Estilizacao from './src/styles/stylesDefault'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={Estilizacao.container}>
       <StatusBar style="auto" />
+      <View style={Estilizacao.headerFooter}>
+      </View>
+      <View style={Estilizacao.body}>
+        <Image style={Estilizacao.image} 
+        source={          
+          require("./src/assets/log_3.png")
+        } 
+        />
+        <Text style={{fontSize:20,paddingBottom:120}}>Seja um Doador de Órgãos!!</Text>
+      </View>
+      <View style={Estilizacao.headerFooter}>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/styles/stylesDefault.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container:{
+      height: '100%'
+    },     
+
+    body: {
+      flex: 7,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent:"space-between",
+    
+      
+    },
+    headerFooter: {
+      backgroundColor: '#34CBC5',
+      flex:1
+    },
+    image: {
+      height: 200,
+      width: 400,
+      
+    } 
+    
+  });

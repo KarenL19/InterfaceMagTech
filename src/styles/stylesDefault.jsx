da301b565baf36813dab0550bfc906085a2e7d4e
Giovana Lira <giovanalfernandes@gmail.com>
2020-08-05 22:15:34 -0300

    Molde de Componente Cabeçalho/Footer

--- src/styles/stylesDefault.jsx
+++ src/styles/stylesDefault.jsx
@@ -91,7 +91,8 @@ export default StyleSheet.create({
       footerComIcones:{
         flexDirection:'row',
         justifyContent: 'space-around',
-        
+        marginTop: -15,
+        marginLeft: 15
       }
     
 

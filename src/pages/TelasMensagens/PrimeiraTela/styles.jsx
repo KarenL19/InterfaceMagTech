ce2258ecb5f2da862ebdd62ac4b3aa24d78d8a3c
Giovana Lira Fernandes <48058149+Giovana300401@users.noreply.github.com>
2020-08-24 22:20:05 -0300

    Ajustando as views no body

--- src/pages/TelasMensagens/PrimeiraTela/styles.jsx
+++ src/pages/TelasMensagens/PrimeiraTela/styles.jsx
@@ -3,29 +3,43 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     height: '100%',
-
+    
   },
 
   body: {
     flex: 7,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'flex-start',
-
+    justifyContent: 'space-between',
+    flexDirection: 'row',
   },
 
   campoDePesquisa:{
-    flex: 1,
-    backgroundColor: 'gray',
+    flex: 8,
+    flexDirection: 'row',
+    height: 100,
+    width: 100,
+    borderColor: 'black',
+    borderWidth: 2,
+    backgroundColor: 'white',
+    marginBottom: 532,
+    //marginLeft: 100,
     
-
   },
 
   viewGeralContatos:{
-
+    flex:20,
+    height: '89%',
+    width: '100%',
+    marginTop: 126,
+    borderColor: 'black',
+    borderWidth: 2,
+    backgroundColor: 'grey',
+    flexDirection: 'row',
+    
   },
 
   viewContatoUnitario:{
-
+   
   },
 });

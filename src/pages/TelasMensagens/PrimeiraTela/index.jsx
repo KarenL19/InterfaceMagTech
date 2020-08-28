c205c2d7fb7b65c6ef72f6c9a7bc62f923476702
Giovana Lira Fernandes <48058149+Giovana300401@users.noreply.github.com>
2020-08-28 00:18:36 -0300

    Ajustes na view campo de pesquisa e adição do icone de mensagens na mesma

--- src/pages/TelasMensagens/PrimeiraTela/index.jsx
+++ src/pages/TelasMensagens/PrimeiraTela/index.jsx
@@ -4,9 +4,13 @@ StatusBar, Text, View, Image,
 import React, { Component } from 'react';
 import EstilizacaoMensagem from './styles';
 
+
 //Import Header and Footer
 import Header from '../../../components/HeaderWithIcons';
 import Footer from '../../../components/FooterWithIcons';
+
+//Import Images
+import iconeAviao from '../../../assets/icons/IconeMensagem.png';
 
 export default class PrimeiraTelaMensagem extends Component {
 render() {
@@ -17,7 +21,12 @@ return (
   <Header/>
   <View style={EstilizacaoMensagem.body}>
     <View style={EstilizacaoMensagem.campoDePesquisa}>
-    
+    <View style={EstilizacaoMensagem.backgroundHeaderIcone}>
+    <Image
+      style={EstilizacaoMensagem.imagemAviaoIcone}
+      source={iconeAviao}
+      />
+     </View> 
     <View style={EstilizacaoMensagem.viewGeralContatos}>
       <View style={EstilizacaoMensagem.viewContatoUnitario}>
       </View>  
--- src/pages/TelasMensagens/PrimeiraTela/styles.jsx
+++ src/pages/TelasMensagens/PrimeiraTela/styles.jsx
@@ -13,26 +13,46 @@ export default StyleSheet.create({
     justifyContent: 'space-between',
     flexDirection: 'row',
   },
-
+  
   campoDePesquisa:{
-    height: 100,
-    width: '100%',
-    borderColor: 'black',
-    borderWidth: 2,
-    backgroundColor: 'white',
-    marginBottom: 532,
-    //marginLeft: 100,
-    
-  },
-
-  viewGeralContatos:{
     height: '100%',
     width: '100%',
     marginTop: 126,
     borderColor: 'black',
     borderWidth: 2,
-    backgroundColor: 'grey',
+    backgroundColor: '#dcdcdc',
+    marginBottom: 126,
     
+  },
+
+  
+  
+  viewGeralContatos:{
+    height: '100%',
+    width: '100%',
+    borderColor: 'black',
+    borderWidth: 2,
+    backgroundColor: 'white',
+    marginTop: 100,
+
+  },
+  
+  backgroundHeaderIcone: {
+    height: 40,
+    width: 40,
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginTop: 20,
+    marginLeft: 163,
+  },
+
+  imagemAviaoIcone: {
+    height: 30,
+    width: 30,
+    margin: 3,
+    marginTop: 5,
   },
 
   viewContatoUnitario:{

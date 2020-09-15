e6fc046e529bc11d322c3ac7a9b78e7a45dff844
Giovana Lira Fernandes <48058149+Giovana300401@users.noreply.github.com>
2020-09-14 22:19:15 -0300

    Colocando o modal sem os ajustes

---
+++ src/components/ModalFeedOpcoes/index.jsx
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import {
+  Alert,
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+  Image
+} from "react-native";
+import Opcoes from "../../assets/icons/botaoOpcoes.png"
+
+
+export default class App extends Component {
+  state = {
+    modalVisible: false
+  };
+
+  setModalVisible = (visible) => {
+    this.setState({ modalVisible: visible });
+  }
+
+  render() {
+    const { modalVisible } = this.state;
+    return (
+      <View>
+        <View style={styles.centeredView}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>Hello World!</Text>
+
+                <TouchableHighlight
+                  style={{ ...styles.openButton, backgroundColor: "#34CBC5" }}
+                  onPress={() => {
+                    this.setModalVisible(!modalVisible);
+                  }}
+                >
+                  <Text style={styles.textStyle}>Salvar Alterações</Text>
+                </TouchableHighlight>
+              </View>
+            </View>
+          </Modal>
+        </View>
+        <View>
+        <TouchableHighlight style={styles.openButton} onPress={() => {
+            this.setModalVisible(true);
+          }}>
+            
+            <Image
+            style={styles.imagemMenu}
+            source={Opcoes}
+          />
+            
+            
+        </TouchableHighlight>
+        </View>
+        </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  openButton: {
+    
+    height: 30,
+    width: 30,
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginRight: 15,
+    marginLeft: 50,
+    marginTop: 30,
+  },
+  textStyle: {
+    color: "black",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  },
+  imagemMenu: {
+    height: 30,
+    width: 30,
+    margin: 1,
+    marginTop: 1,
+  }
+
+});
--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -10,34 +10,39 @@ import iconPerfil from '../../assets/icons/fotoPerfil.png';
 import iconAlerta from '../../assets/icons/botaoAlerta.png';
 import iconOpcoes from '../../assets/icons/botaoOpcoes.png';
 
+//Import components
+import ModalOpcoes from '../../components/ModalFeedOpcoes';
+
+
+//Import styles
 import styles from './styles';
 
+
+
 function TelaFeed() {
-  return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.body}>
-        <View style={styles.postContainer}>
-          <View style={styles.postHeader}>
-            <Image
-              source={iconPerfil}
-              style={styles.backgroundPerfil}
-            />
-            <Text style={styles.usernameText}>Nome do Usuário</Text>
-            <Image
-              source={iconAlerta}
-              style={styles.backgroundIcones}
-            />
-            <Image
-              source={iconOpcoes}
-              style={styles.backgroundIcones}
-            />
-          </View>
+return (
+  <View style={styles.container}>
+    <Header />
+    <View style={styles.body}>
+      <View style={styles.postContainer}>
+        <View style={styles.postHeader}>
+          <Image
+            source={iconPerfil}
+            style={styles.backgroundPerfil}
+          />
+          <Text style={styles.usernameText}>Nome do Usuário</Text>
+          <Image
+            source={iconAlerta}
+            style={styles.backgroundIcones}
+          />
+          <ModalOpcoes/>
+          
         </View>
       </View>
-      <Footer />
     </View>
-  );
+    <Footer />
+  </View>
+);
 }
 
 export default TelaFeed;

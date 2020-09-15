e3913fa39baa6ace9519aab61f68f1d80357434a
CarolineFontainha <cbsfontainha@gmail.com>
2020-09-15 01:21:26 -0300

    :tada: organizing content from the modal conta

--- src/components/ModalConfgConta/index.jsx
+++ src/components/ModalConfgConta/index.jsx
@@ -5,8 +5,13 @@ import {
   StyleSheet,
   Text,
   TouchableHighlight,
-  View
+  View,
+  Image,
+  TextInput,
 } from "react-native";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import styles from "./styles"
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default class App extends Component {
   state = {
@@ -32,15 +37,39 @@ export default class App extends Component {
           >
             <View style={styles.centeredView}>
               <View style={styles.modalView}>
-                <Text style={styles.modalText}>Hello World!</Text>
+              <KeyboardAwareScrollView>
+          
+            <View style={styles.viewInput}>
+            <Text style={styles.titleInput}>E-mail:</Text>
+              <TextInput
+                placeholder="example@example.com"
+                style={styles.input}
+                placeholderTextColor="#000"
+              />
+              <Text style={styles.titleInput}>Número de telefone:</Text>
+              <TextInput
+                placeholder="(xx) xxxxx-xxxx"
+                style={styles.input}
+                placeholderTextColor="#000"
+              />             
+              </View>          
+        </KeyboardAwareScrollView>
+        <View style={styles.viewTouch}>
+        <TouchableOpacity style={styles.touch}>
+        <Text>Gerenciar conta</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.touch}>
+        <Text>Apagar conta</Text>
+        </TouchableOpacity>
+        </View>
 
                 <TouchableHighlight
-                  style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+                  style={styles.closeButton}
                   onPress={() => {
                     this.setModalVisible(!modalVisible);
                   }}
                 >
-                  <Text style={styles.textStyle}>Fechar Modal</Text>
+                  <Text style={styles.textStyle}>Salvar Alterações</Text>
                 </TouchableHighlight>
               </View>
             </View>
@@ -61,44 +90,3 @@ export default class App extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
-  },
-  
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5
-  },
-  openButton: {
-    backgroundColor: "#FFE6E6",
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-    borderWidth: 1
-  },
-  textStyle: {
-    color: "black",
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center"
-  }
-});
---
+++ src/components/ModalConfgConta/styles.jsx
@@ -0,0 +1,81 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+  input: {
+    height: 40,
+    width: 200,
+    borderWidth: 1,
+    textAlign: 'center',
+    marginTop: 10,
+    backgroundColor: '#fff',
+
+  },
+  touch: {
+    height: 40,
+    textAlign: 'center',
+    marginTop: 10,
+    borderBottomWidth: 2,
+    backgroundColor: '#fff',
+    width: 150,
+    borderColor: '#dcdcdc',
+    alignItems: 'center',
+  },
+
+  viewInput: {
+    alignItems: 'center',
+  },
+  closeButton: {
+    backgroundColor: '#34CBC5',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    borderWidth: 1,
+    marginTop: 50,
+  },
+  viewTouch: {
+    marginTop: 50,
+  },
+  titleInput: {
+    marginTop: 30,
+  },
+  centeredView: {
+    flex: 1,
+    alignItems: 'stretch',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 50,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    flexDirection: 'column',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  openButton: {
+    backgroundColor: '#FFE6E6',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    borderWidth: 1,
+  },
+  textStyle: {
+    color: 'black',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
+
+export default styles;
